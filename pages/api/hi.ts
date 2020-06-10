434ab5d88351375f1a992456073a4a858c186e69
Josh Pollock <jpollock412@gmail.com>
2020-06-10 13:48:55 -0400

    Basic hi roy commit

---
+++ pages/api/hi.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+	res.statusCode = 200;
+	res.setHeader("Content-Type", "application/json");
+	res.end(JSON.stringify({ hi: "Roy" }));
+};
